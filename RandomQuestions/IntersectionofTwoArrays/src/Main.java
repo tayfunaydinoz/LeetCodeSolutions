cfd453cef64aac057bbbe052b1a80e921d2674b7
Tayfun AYDINÖZ <tayfun.aydinoz@hotmail.com>
2023-09-27 23:55:03 +0300

    Intersection of Two Arrays is solved

---
+++ RandomQuestions/IntersectionofTwoArrays/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums1 ={1,2,2,1};
+        int[] nums2 ={2,2};
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.intersection(nums1,nums2)));
+    }
+}
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i=0;i<nums1.length;i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j] && !result.contains(nums1[i])) {
+                    result.add(nums1[i]);
+                    break;
+                }
+            }
+        }
+        int[] resultarray = new int[result.size()];
+        int i = 0;
+        for (Integer e : result){
+            resultarray[i++] = e;
+        }
+
+        return resultarray;
+    }
+}
